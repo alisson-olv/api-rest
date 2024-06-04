c202f85dd5226dff5bb596ddd481a4a5697e593a
Alisson <binho_alisson@hotmail.com>
2024-06-04 15:14:33 -0300

    feat: implementing route create to /cities

--- src/server/controllers/cities/Create.ts
+++ src/server/controllers/cities/Create.ts
@@ -1,43 +1,23 @@
-import { Request, RequestHandler, Response } from 'express';
-import { StatusCodes } from 'http-status-codes';
+import { Request, Response } from 'express';
 import * as yup from 'yup';
+import { StatusCodes } from 'http-status-codes';
+
 import { validation } from '../../shared/middleware';
 
 interface ICities {
   name: string;
-  state: string;
 }
 
-const bodyValidation: yup.ObjectSchema<ICities> = yup.object().shape({
-  name: yup.string().required().min(3),
-  state: yup.string().required().min(3),
-});
-
-export const createBodyValidator: RequestHandler = async (req, res, next) => {
-  try {
-    await bodyValidation.validate(req.body, {
-      abortEarly: false,
-    });
-    return next();
-  } catch (error) {
-    const yupError = error as yup.ValidationError;
-    const validationErrors: Record<string, string> = {};
-
-    yupError.inner.map((error) => {
-      if (!error.path) return;
-      validationErrors[error.path] = error.message;
-    });
-
-    return res.status(StatusCodes.BAD_REQUEST).json({
-      errors: validationErrors,
-    });
-  }
-};
-
-export const createValidation = validation();
+export const createValidation = validation((getSchema) => ({
+  body: getSchema<ICities>(
+    yup.object().shape({
+      name: yup.string().required().min(3),
+    })
+  ),
+}));
 
 export const create = async (req: Request<{}, {}, ICities>, res: Response) => {
   console.log(req.body);
 
-  return res.send('Create');
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('Não implementado');
 };
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -10,7 +10,7 @@ router.get('/', (req, res) => {
 
 router.post(
   '/cities',
-  CitiesController.createBodyValidator,
+  CitiesController.createValidation,
   CitiesController.create
 );
 
