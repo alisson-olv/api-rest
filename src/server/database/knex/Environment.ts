018d51afa7c5ab105871ad187f5881934a4ecc23
Alisson <binho_alisson@hotmail.com>
2024-06-07 20:07:32 -0300

    feat: creating config to use knex.js as query builder

---
+++ src/server/database/knex/Environment.ts
@@ -0,0 +1,39 @@
+import { Knex } from 'knex';
+import path from 'path';
+import { Database } from 'sqlite';
+
+export const development: Knex.Config = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  connection: {
+    filename: path.resolve(
+      __dirname,
+      '..',
+      '..',
+      '..',
+      '..',
+      'database.sqlite'
+    ),
+  },
+  migrations: {
+    directory: path.resolve(__dirname, '..', 'migrations'),
+  },
+  seeds: {
+    directory: path.resolve(__dirname, '..', 'seeds'),
+  },
+  pool: {
+    afterCreate: (connection: Database, done: Function) => {
+      connection.run('PRAGMA foreign_keys = ON');
+      done();
+    },
+  },
+};
+
+export const test: Knex.Config = {
+  ...development,
+  connection: ':memory:',
+};
+
+export const production: Knex.Config = {
+  ...development,
+};
