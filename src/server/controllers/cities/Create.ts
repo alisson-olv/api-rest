3ecb8211732656d7a988737f03b04b2f27b85fda
Alisson <binho_alisson@hotmail.com>
2024-06-07 22:54:15 -0300

    feat: creating provider to /cities

--- src/server/controllers/cities/Create.ts
+++ src/server/controllers/cities/Create.ts
@@ -4,17 +4,28 @@ import { StatusCodes } from 'http-status-codes';
 
 import { validation } from '../../shared/middleware';
 import { ICity } from '../../database/models';
+import { CitiesProvider } from '../../database/providers/cities';
 
 interface IBodyProps extends Omit<ICity, 'id'> {}
 
 export const createValidation = validation((getSchema) => ({
   body: getSchema<IBodyProps>(
     yup.object().shape({
-      name: yup.string().required().min(3),
+      name: yup.string().required().min(3).max(150),
     })
   ),
 }));
 
 export const create = async (req: Request<{}, {}, ICity>, res: Response) => {
-  return res.status(StatusCodes.CREATED).json(1);
+  const result = await CitiesProvider.create(req.body);
+
+  if (result instanceof Error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      errors: {
+        default: result.message,
+      },
+    });
+  }
+
+  return res.status(StatusCodes.CREATED).json(result);
 };
