982766e10b1c9144c4bc5a2b0213fccdb08e960d
Alisson <binho_alisson@hotmail.com>
2024-06-08 23:05:15 -0300

    feat: adding config to sqlite

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 import { server } from './server/Server';
+import { Knex } from './server/database/knex';
 const port = process.env.PORT || 3000;
 
-server.listen(port, () => {
-  console.log('Server running on port ' + port);
-});
+const startServer = () => {
+  server.listen(port, () => {
+    console.log('Server running on port ' + port);
+  });
+};
+
+if (process.env.IS_LOCALHOST !== 'true') {
+  Knex.migrate
+    .latest()
+    .then(() => {
+      startServer();
+    })
+    .catch(console.log);
+}
