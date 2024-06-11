512ed58984d7d4b713add52f5fa22ac893182777
Alisson <binho_alisson@hotmail.com>
2024-06-11 17:39:37 -0300

    feat: creating controllers and routes to /persons

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { CitiesController } from '../controllers';
+import { CitiesController, PersonsController } from '../controllers';
 
 const router = Router();
 
@@ -8,6 +8,7 @@ router.get('/', (req, res) => {
   return res.send('Ola');
 });
 
+// /CITIES
 router.post(
   '/cities',
   CitiesController.createValidation,
@@ -36,6 +37,37 @@ router.delete(
   '/cities/:id',
   CitiesController.deleteByIdValidation,
   CitiesController.deleteById
+);
+
+// /PERSONS
+router.post(
+  '/persons',
+  PersonsController.createValidation,
+  PersonsController.create
+);
+
+router.get(
+  '/persons',
+  PersonsController.getAllValidation,
+  PersonsController.getAll
+);
+
+router.get(
+  '/persons/:id',
+  PersonsController.getByIdValidation,
+  PersonsController.getById
+);
+
+router.put(
+  '/persons/:id',
+  PersonsController.updateByIdValidation,
+  PersonsController.updateById
+);
+
+router.delete(
+  '/persons/:id',
+  PersonsController.deleteByIdValidation,
+  PersonsController.deleteById
 );
 
 export { router };
